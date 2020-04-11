20bbb5af75356c1e30dc2d5d481671837ecd49c7
hackemate_ninja <nitrowitty@gmail.com>
2020-04-11 05:21:24 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,46 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+  },
+  {
+    path: 'blog',
+    loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule)
+  },
+  {
+    path: 'videos',
+    loadChildren: () => import('./videos/videos.module').then(m => m.VideosModule)
+  },
+  {
+    path: 'portafolio',
+    loadChildren: () => import('./portafolio/portafolio.module').then(m => m.PortafolioModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
+  },
+  {
+    path: '404',
+    loadChildren: () => import('./lost404/lost404.module').then(m => m.Lost404Module)
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
